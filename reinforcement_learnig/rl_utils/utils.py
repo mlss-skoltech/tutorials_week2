27f339ebf5fd68671bd6e2419cdfd1d3298e1783
NinaMaz <ninam@MacBook-Pro-Nina.local>
2019-09-03 14:03:52 +0300

    Create utils.py

---
+++ reinforcement_learnig/rl_utils/utils.py
@@ -0,0 +1,118 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import gym
+from gym import logger as gymlogger
+from gym.wrappers import Monitor
+gymlogger.set_level(40) #error only
+import math
+import glob
+import io
+import base64
+from IPython.display import HTML
+
+from IPython import display as ipythondisplay
+
+def plot_values(V):
+    # reshape value function
+    str_layout = [['S', 'F', 'F', 'F'], ['F', 'H', 'F', 'H'], ['F', 'F', 'F', 'H'], ['H', 'F', 'F', 'G']]
+    V_sq = np.reshape(V, (4,4))
+    # plot the state-value function
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111)
+    im = ax.imshow(V_sq, cmap='cool')
+    for (((j,i),label),(_,l)) in zip(np.ndenumerate(V_sq), np.ndenumerate(str_layout)):
+        ax.text(i, j, np.round(label, 5), ha='center', va='bottom', fontsize=14)
+        ax.text(i, j, l, ha='center', va='top', fontsize=14,  color = 'purple')
+    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+    plt.title('State-Value Function')
+    plt.show()
+
+def plot_blackjack_values(V):
+
+    def get_Z(x, y, usable_ace):
+        if (x,y,usable_ace) in V:
+            return V[x,y,usable_ace]
+        else:
+            return 0
+
+    def get_figure(usable_ace, ax):
+        x_range = np.arange(11, 22)
+        y_range = np.arange(1, 11)
+        X, Y = np.meshgrid(x_range, y_range)
+        
+        Z = np.array([get_Z(x,y,usable_ace) for x,y in zip(np.ravel(X), np.ravel(Y))]).reshape(X.shape)
+
+        surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.coolwarm, vmin=-1.0, vmax=1.0)
+        ax.set_xlabel('Player\'s Current Sum')
+        ax.set_ylabel('Dealer\'s Showing Card')
+        ax.set_zlabel('State Value')
+        ax.view_init(ax.elev, -120)
+
+    fig = plt.figure(figsize=(20, 20))
+    ax = fig.add_subplot(211, projection='3d')
+    ax.set_title('Usable Ace')
+    get_figure(True, ax)
+    ax = fig.add_subplot(212, projection='3d')
+    ax.set_title('No Usable Ace')
+    get_figure(False, ax)
+    plt.show()
+
+def plot_policy(policy):
+
+    def get_Z(x, y, usable_ace):
+        if (x,y,usable_ace) in policy:
+            return policy[x,y,usable_ace]
+        else:
+            return 1
+
+    def get_figure(usable_ace, ax):
+        x_range = np.arange(11, 22)
+        y_range = np.arange(10, 0, -1)
+        X, Y = np.meshgrid(x_range, y_range)
+        Z = np.array([[get_Z(x,y,usable_ace) for x in x_range] for y in y_range])
+        surf = ax.imshow(Z, cmap=plt.get_cmap('Pastel2', 2), vmin=0, vmax=1, extent=[10.5, 21.5, 0.5, 10.5])
+        plt.xticks(x_range)
+        plt.yticks(y_range)
+        plt.gca().invert_yaxis()
+        ax.set_xlabel('Player\'s Current Sum')
+        ax.set_ylabel('Dealer\'s Showing Card')
+        ax.grid(color='w', linestyle='-', linewidth=1)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.1)
+        cbar = plt.colorbar(surf, ticks=[0,1], cax=cax)
+        cbar.ax.set_yticklabels(['0 (STICK)','1 (HIT)'])
+            
+    fig = plt.figure(figsize=(15, 15))
+    ax = fig.add_subplot(121)
+    ax.set_title('Usable Ace')
+    get_figure(True, ax)
+    ax = fig.add_subplot(122)
+    ax.set_title('No Usable Ace')
+    get_figure(False, ax)
+    plt.show()
+    
+"""
+Utility functions to enable video recording of gym environment and displaying it
+To enable video, just do "env = wrap_env(env)""
+"""
+
+def show_video():
+    mp4list = glob.glob('video/*.mp4')
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, 'r+b').read()
+        encoded = base64.b64encode(video)
+        ipythondisplay.display(HTML(data='''<video alt="test" autoplay 
+                    loop controls style="height: 400px;">
+                    <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+                 </video>'''.format(encoded.decode('ascii'))))
+    else: 
+        print("Could not find video")
+    
+
+def wrap_env(env):
+    env = Monitor(env, './video', force=True)
+    return env
